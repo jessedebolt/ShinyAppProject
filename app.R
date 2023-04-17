c49180feaaffdb158f0f2aa4ed8952b5cd90711f
jessedebolt <jessedebolt@comcast.net>
2023-04-16 21:38:22 -0700

    Add archive to ignore, move folder

---
+++ app.R
@@ -0,0 +1,122 @@
+# Load libraries
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(shinyWidgets)
+library(DT)
+library(shinythemes)
+
+# Load data
+college <- read.csv("../../../../data/college_pell2.csv")
+
+# User Interface
+ui <- fluidPage(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.min.css")
+  ),
+#  themeSelector(),
+
+    # Application title
+    titlePanel("US College Selection"),
+        
+    # Sidebar with a selectors, etc. for data filtering
+    sidebarLayout(
+        sidebarPanel(
+          # Drop down for state
+          pickerInput("state", "State", 
+                      choices = unique(college$state),
+                      options = list(`actions-box` = TRUE),
+#                      selected = unlist(college$state), 
+                      selected = "OR",
+                      multiple = TRUE),
+
+          # Drop down for public/private
+          pickerInput("ownership", "Public or Private", 
+                      choices = unique(college$ownership),
+                      options = list(`actions-box` = TRUE),
+#                      selected = unlist(college$ownership),
+                      selected = "Public",
+                      multiple = TRUE),
+          
+          # Drop down for adminRate          
+          pickerInput("gender","Coed or Male/Female", 
+                      choices = unique(college$gender), 
+                      options = list(`actions-box` = TRUE),
+                      selected = "COED",
+                      multiple = FALSE),
+
+          # Checkbox for graduate
+          checkboxInput("graduate", "Graduate program available",
+                        value = FALSE), 
+          
+          # Add submit button for reactive update
+          submitButton("Update Tabs")),
+        
+        mainPanel(
+          # Main panel with two tabs
+          tabsetPanel(
+            tabPanel("Map", leafletOutput("map")),
+            tabPanel("Datatable", DT::dataTableOutput("datatable"))
+          )
+        )
+    )
+)
+
+
+
+# Define server logic
+server <- function(input, output) {
+
+    output$map <- renderLeaflet({
+
+      data_filtered <- college %>%
+        filter(state == input$state,
+               ownership == input$ownership,
+               gender == input$gender,
+               graduate_program == input$graduate
+        )
+
+      
+        leaflet(data_filtered) %>%
+        addTiles() %>%
+        addMarkers(~longitude, ~latitude, popup = ~school_name)
+          # addMarkers(~longitude, ~latitude, 
+          #            popup = paste(college$school_name, "<br>",
+          #                          "Website:", college$school_webpage))
+        })
+    
+    
+    # Create a DT::datatable
+    output$datatable <- DT::renderDataTable({
+      
+      data_filtered <- college %>%
+        filter(state == input$state,
+               ownership == input$ownership,
+               gender == input$gender,
+               graduate_program == input$graduate
+        )
+      
+      datatable(data = data_filtered, caption = 'US colleges with noted criteria',
+                extensions = c("Scroller", "Buttons"), 
+                options = list(autoFill = TRUE, pageLength = 5, 
+                               fixedColumns.left = 2, # !!! NOT working
+                               deferRender = TRUE, rownames = FALSE, 
+                               scrollX = TRUE, scrollY = "500px",
+                               scroller = TRUE, dom = "Bfrtip",
+                               buttons = c("copy", "csv", "excel", "pdf", "print"),
+                               columnDefs = list(list(className = 'dt-head-center',
+                                                      targets = 0:47),
+                                                 list(className = 'dt-body-left',
+                                                      targets = 1:47))),
+                               
+                class = 'cell-boarder stripe hover'
+                
+                ) %>%
+        formatStyle(columns = c(1:47), 
+                     fontWeight = "bold")
+    })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
